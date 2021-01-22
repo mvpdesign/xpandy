c7b3066c46db36c90f38d3d800a9a9cdb17169bb
heychris <senorboo@gmail.com>
2021-01-22 16:26:54 -0600

    Add rollup uglify. Add build for polyfill version

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import { uglify } from "rollup-plugin-uglify";
 
 import { main, version } from "./package.json";
 
 const config = {
-	plugins: [typescript(), commonjs(), resolve()],
+	plugins: [typescript(), commonjs(), uglify(), resolve()],
 	watch: {
 		include: "src/**"
 	}
@@ -36,6 +37,34 @@ From MVP Marketing + Design
 			file: main,
 			format: "umd",
 			name: "xpandy",
+			sourcemap: true
+		},
+		...config
+  },
+  {
+		input: "src/index.polyfills.ts",
+		output: {
+			banner: `/*
+XXXXXXXXXXXXXXXXXXXX$$$$$$$$$$$$$$$$$$$$
+-#XXXXXXXXXXXXXXXXX///$$$$$$$$$$$$$$$$$-
+---XXXXXXXXXXXXXX$/////$$$$$$$$$$$$$$#--
+----$XXXXXXXXXXX/////////$$$$$$$$$$$----
+------XXXXXXXXX///////////$$$$$$$$%-----
+-------%XXXXX$/////////////$$$$$$-------
+---------XXX/////////////////$$&--------
+----------#&&&&&&&&&&&&&&&&&&&----------
+------------&&&&&&&&&&&&&&&&#-----------
+-------------#&&&&&&&&&&&&&&------------
+---------------&&&&&&&&&&#--------------
+----------------&&&&&&&&%---------------
+-----------------&&&&&&-----------------
+-------------------#&#------------------
+Xpandy ${version}
+From MVP Marketing + Design
+*/`,
+			file: 'dist/xpandy-polyfill.js',
+			format: "umd",
+			name: "xpandy-polyfill",
 			sourcemap: true
 		},
 		...config
